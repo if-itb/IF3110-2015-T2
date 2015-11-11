27216304e8a2985480c9a12f6b3d0882b2fe1c3d
feryandi <feryandi.n@gmail.com>
2015-11-11 10:30:03 +0700

    GlassFish Server

--- StackExchange WS/src/java/QuestionModel/QuestionWS.java
+++ StackExchange WS/src/java/QuestionModel/QuestionWS.java
@@ -5,9 +5,17 @@
  */
 package QuestionModel;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
+import Database.DB;
 
 /**
  *
@@ -16,11 +24,37 @@ import javax.jws.WebParam;
 @WebService(serviceName = "QuestionWS")
 public class QuestionWS {
 
+  Connection conn = DB.connect();  
+  
   /**
    * This is a sample web service operation
    */
   @WebMethod(operationName = "hello")
   public String hello(@WebParam(name = "name") String txt) {
-    return "Hello " + txt + " !";
+    int ret = -1;
+    try {      
+      Statement stmt = conn.createStatement();
+      String sql;
+      sql = "SELECT * FROM questions WHERE topic = ?";
+      PreparedStatement dbStatement = conn.prepareStatement(sql);
+      dbStatement.setString(1, txt);
+      
+      ResultSet rs = dbStatement.executeQuery();
+      
+      // Extract data from result set
+      while(rs.next()){
+        //Retrieve by column name
+        int id = rs.getInt("id");
+        
+        ret = id;   
+      }
+      
+      rs.close();
+      stmt.close();
+      conn.close();
+    } catch (SQLException ex) {
+      Logger.getLogger(QuestionWS.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    return "Yea its:" + ret;
   }
 }
