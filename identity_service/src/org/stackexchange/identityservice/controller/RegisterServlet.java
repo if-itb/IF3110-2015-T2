3a52caaa069bc608a2fdd2c8b54dc8bf36f0102c
Vincent The <vincent.s.the@gmail.com>
2015-11-12 10:46:42 +0700

    bootstrap identity service

---
+++ identity_service/src/org/stackexchange/identityservice/controller/RegisterServlet.java
@@ -0,0 +1,7 @@
+package org.stackexchange.identityservice.controller;
+
+import javax.servlet.http.HttpServlet;
+
+public class RegisterServlet extends HttpServlet {
+
+}
---
+++ identity_service/src/org/stackexchange/identityservice/model/UserModel.java
@@ -0,0 +1,5 @@
+package org.stackexchange.identityservice.model;
+
+public class UserModel {
+
+}
