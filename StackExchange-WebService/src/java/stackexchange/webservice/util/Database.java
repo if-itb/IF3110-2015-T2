146acfa8faec22e47e6c5aab295a59d1ea4cad46
Fauzan Rifqy <fauzanriff@gmail.com>
2015-11-14 16:16:37 +0700

    Add utility package
    Update :
     - Add utility package include : Database.java, Exception.java,
    Warning.java

---
+++ StackExchange-WebService/src/java/stackexchange/webservice/util/Database.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stackexchange.webservice.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author fauzanrifqy
+ */
+public class Database {
+    private Connection connection;
+    private String userName = "root";
+    private String passWord = "root";
+    private String hostName = "localhost";
+    private String dbName = "StackExchange";
+    
+    public Database(){
+        try {
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+        } catch (SQLException e) {
+            throw new NestedException("Error driver : " + e.getMessage(), e, 0);
+        }
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://" + hostName + ":3306/" + dbName + "?user=" + userName + "&password=" + passWord);
+        } catch (Exception e) {
+            throw new NestedException("Error connection : " + e.getMessage(), e, 0);
+        }
+    }
+    
+    public Connection getConnection() {
+        return connection;
+    }
+    
+    public void closeConnection() {
+        try {
+            connection.close();
+        } catch (Exception e) {
+            throw new NestedException("Error close connection : " + e.getMessage(), e, 0);
+        }
+    }
+}
