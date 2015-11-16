efeeba242481d3a4b6155b5c54d2bbd02ff4d144
pipinkurniawati <13513089@std.stei.itb.ac.id>
2015-11-16 20:50:28 +0700

    update db scheme

--- stackexchange.sql
+++ stackexchange.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 16, 2015 at 12:48 PM
+-- Generation Time: Nov 16, 2015 at 02:48 PM
 -- Server version: 5.6.21
 -- PHP Version: 5.6.3
 
@@ -58,11 +58,32 @@ CREATE TABLE IF NOT EXISTS `question` (
 
 CREATE TABLE IF NOT EXISTS `user` (
 `id` int(11) NOT NULL,
-  `username` varchar(255) NOT NULL,
   `password` varchar(32) NOT NULL,
   `name` varchar(255) NOT NULL,
   `email` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vote_answer`
+--
+
+CREATE TABLE IF NOT EXISTS `vote_answer` (
+  `id_user` int(11) NOT NULL,
+  `id_answer` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vote_question`
+--
+
+CREATE TABLE IF NOT EXISTS `vote_question` (
+  `id_user` int(11) NOT NULL,
+  `id_question` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
@@ -84,7 +105,19 @@ ALTER TABLE `question`
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `username` (`username`);
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `vote_answer`
+--
+ALTER TABLE `vote_answer`
+ ADD KEY `id_answer` (`id_answer`);
+
+--
+-- Indexes for table `vote_question`
+--
+ALTER TABLE `vote_question`
+ ADD KEY `id_question` (`id_question`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -121,6 +154,18 @@ ADD CONSTRAINT `answer_user` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`) ON
 --
 ALTER TABLE `question`
 ADD CONSTRAINT `question_user` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `vote_answer`
+--
+ALTER TABLE `vote_answer`
+ADD CONSTRAINT `vote-answer` FOREIGN KEY (`id_answer`) REFERENCES `answer` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `vote_question`
+--
+ALTER TABLE `vote_question`
+ADD CONSTRAINT `vote-question` FOREIGN KEY (`id_question`) REFERENCES `question` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
