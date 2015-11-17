8e64ea11693bf94a04ea3d03d00d403b1e31734c
Rahman Adianto <rahman.adianto@gmail.com>
2015-11-17 12:10:06 +0700

    Refactor Database connection

--- StackExchangeService/src/java/AnswerModel/Answer.java
+++ StackExchangeService/src/java/AnswerModel/Answer.java
@@ -3,30 +3,30 @@ package AnswerModel;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
-@XmlRootElement(name="Answer")  // nama elemen SOAP
+@XmlRootElement(name="Answer")  
 public class Answer {
     
-    @XmlElement(name="id", required=true)
-    private int id;
-    @XmlElement(name="id_question", required=true)
+    @XmlElement(name= "id", required = true)
+    private int id_answer;
+    @XmlElement(name= "id_question", required = true)
     private int id_question;
-    @XmlElement(name="vote", required=true)
+    @XmlElement(name= "vote", required = true)
     private int vote;
-    @XmlElement(name="content", required=true)
+    @XmlElement(name = "content", required = true)
     private String content;
-    @XmlElement(name="date", required=true)
+    @XmlElement(name = "date", required = true)
     private String date;
-    @XmlElement(name="username", required=true)
+    @XmlElement(name = "username", required = true)
     private String username;
     
     public Answer() {
-        id = 0;
+        id_answer = 0;
         id_question = 0;
         vote = 0;
     }
     
-    public Answer(int id, int id_question, int vote, String content, String date, String username) {
-        this.id = id;
+    public Answer(int id_answer, int id_question, int vote, String content, String date, String username) {
+        this.id_answer = id_answer;
         this.id_question = id_question;
         this.vote = vote;
         this.content = content;
@@ -37,5 +37,8 @@ public class Answer {
     public String getContent() {
         return content;
     }
-
+    
+    public String getUsername() {
+        return username;
+    }
 }
