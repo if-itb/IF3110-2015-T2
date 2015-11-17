2b9b337170878f2ebe5d86ec44654434d5275f7b
angelynz95 <angel.lynz95@gmail.com>
2015-11-17 19:35:11 +0700

    Add Question Web Services

--- Stackexchange WS/src/java/QuestionModel/QuestionWS.java
+++ Stackexchange WS/src/java/QuestionModel/QuestionWS.java
@@ -5,22 +5,144 @@
  */
 package QuestionModel;
 
-import javax.jws.WebService;
-import javax.jws.WebMethod;
-import javax.jws.WebParam;
+import database.Database;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.jws.*;
 
 /**
  *
- * @author Devina
+ * @author Irene Wiliudarsan - 13513002
+ * @author Angela Lynn - 13513032
+ * @author Devina Ekawati - 13513088
  */
 @WebService(serviceName = "QuestionWS")
 public class QuestionWS {
+  // Connect ke database
+  private Database database = new Database();
 
   /**
-   * This is a sample web service operation
+   * Web service operation
    */
-  @WebMethod(operationName = "hello")
-  public String hello(@WebParam(name = "name") String txt) {
-    return "Hello " + txt + " !";
+  @WebMethod(operationName = "getQuestions")
+  @WebResult(name="Question")
+  public ArrayList<Question> getQuestions() {
+    //TODO write your implementation code here:
+    ArrayList<Question> questions = new ArrayList<Question>();
+    
+    try {
+      // Connect database
+      Connection connection = database.connectDatabase();
+      Statement statement = connection.createStatement();
+      
+      // Menjalankan query
+      String query = "SELECT * FROM question ORDER BY datetime DESC";
+      PreparedStatement databaseStatement = connection.prepareStatement(query);
+      ResultSet result = databaseStatement.executeQuery();
+      
+      // Mengambil semua data hasil eksekusi query
+      while (result.next()) {
+        questions.add(new Question( result.getInt("id_question"),
+                                    result.getString("topic"),
+                                    result.getString("content"),
+                                    result.getString("datetime"),
+                                    result.getInt("id_user")));
+      }
+      
+      result.close();
+      statement.close();
+    } catch (SQLException ex) {
+      Logger.getLogger(QuestionWS.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    
+    return questions;
+  }
+  
+  @WebMethod(operationName = "addQuestion")
+  public boolean addQuestions(@WebParam(name = "topic") String topic, @WebParam(name = "content") String content, @WebParam(name = "id_user") int idUser) {
+    //TODO write your implementation code here:
+    boolean questionAdded;
+    
+    try {
+      // Connect database
+      Connection connection = database.connectDatabase();
+      Statement statement = connection.createStatement();
+      
+      // Menjalankan query
+      String query = "INSERT INTO question (topic, content, id_user) VALUES (?, ?, ?)";
+      PreparedStatement databaseStatement = connection.prepareStatement(query);
+      databaseStatement.setString(1, topic);
+      databaseStatement.setString(2, content);
+      databaseStatement.setInt(3, idUser);
+      databaseStatement.executeUpdate();
+      
+      statement.close();
+      questionAdded = true;
+    } catch (SQLException ex) {
+      Logger.getLogger(QuestionWS.class.getName()).log(Level.SEVERE, null, ex);
+      questionAdded = false;
+    }
+    
+    return questionAdded;
+  }
+  
+  @WebMethod(operationName = "editQuestion")
+  public boolean editQuestions(@WebParam(name = "id_question") int idQuestion, @WebParam(name = "topic") String topic, @WebParam(name = "content") String content) {
+    //TODO write your implementation code here:
+    boolean questionEdited;
+    
+    try {
+      // Connect database
+      Connection connection = database.connectDatabase();
+      Statement statement = connection.createStatement();
+      
+      // Menjalankan query
+      String query = "UPDATE question SET topic = ?, content = ? WHERE id_question = ?";
+      PreparedStatement databaseStatement = connection.prepareStatement(query);
+      databaseStatement.setString(1, topic);
+      databaseStatement.setString(2, content);
+      databaseStatement.setInt(3, idQuestion);
+      databaseStatement.executeUpdate();
+      
+      statement.close();
+      questionEdited = true;
+    } catch (SQLException ex) {
+      Logger.getLogger(QuestionWS.class.getName()).log(Level.SEVERE, null, ex);
+      questionEdited = false;
+    }
+    
+    return questionEdited;
+  }
+  
+  @WebMethod(operationName = "deleteQuestion")
+  public boolean deleteQuestions(@WebParam(name = "id_question") int idQuestion) {
+    //TODO write your implementation code here:
+    boolean questionDeleted;
+    
+    try {
+      // Connect database
+      Connection connection = database.connectDatabase();
+      Statement statement = connection.createStatement();
+      
+      // Menjalankan query
+      String query1 = "DELETE FROM question WHERE id_question = ?";
+      String query2 = "DELETE FROM answer WHERE id_question = ?";
+      PreparedStatement databaseStatement1 = connection.prepareStatement(query1);
+      PreparedStatement databaseStatement2 = connection.prepareStatement(query2);
+      databaseStatement1.setInt(1, idQuestion);
+      databaseStatement2.setInt(1, idQuestion);
+      databaseStatement1.executeUpdate();
+      databaseStatement2.executeUpdate();
+      
+      statement.close();
+      questionDeleted = true;
+    } catch (SQLException ex) {
+      Logger.getLogger(QuestionWS.class.getName()).log(Level.SEVERE, null, ex);
+      questionDeleted = false;
+    }
+    
+    return questionDeleted;
   }
 }
