142a9414aacaefb8f63f23dc41eaf2e514d72aaf
elvanowen <vanzz_95@hotmail.com>
2015-11-18 03:15:11 +0700

    Created servlet for answer

---
+++ StackExchange_FrontEnd/src/models/Answer.java
@@ -0,0 +1,36 @@
+package models;
+
+import java.util.HashMap;
+
+/**
+ * Created by elvan_owen on 11/18/15.
+ */
+public class Answer {
+    private String content;
+    private String createDate;
+    private Integer userId;
+    private Integer vote;
+
+    public Answer(HashMap<String, String> data){
+        this.content = data.get("content");
+        this.createDate = data.get("createDate");
+        this.userId = Integer.parseInt(data.get("userId"));
+        this.vote = Integer.parseInt(data.get("vote"));
+    }
+
+    public String getContent(){
+        return content;
+    }
+
+    public String getCreateDate(){
+        return createDate;
+    }
+
+    public Integer getVote(){
+        return vote;
+    }
+
+    public Integer getUserId(){
+        return userId;
+    }
+}
