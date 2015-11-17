f616be01da44381984750b581fddadc36c3a40a4
elvanowen <vanzz_95@hotmail.com>
2015-11-17 23:00:53 +0700

    Created util function for communicating with webservices and parse the response

---
+++ StackExchange_FrontEnd/src/util/HttpRequest.java
@@ -0,0 +1,80 @@
+package util;
+
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+/**
+ * Created by elvan_owen on 11/17/15.
+ */
+public class HttpRequest {
+    public static String executeGET(String targetURL) throws IOException{
+        URL yahoo = new URL(targetURL);
+        URLConnection yc = yahoo.openConnection();
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(
+                        yc.getInputStream()));
+        String inputLine;
+        String res = "";
+
+        while ((inputLine = in.readLine()) != null)
+            res += inputLine;
+
+        return res;
+    }
+
+    public static String executeMethod(String method, HashMap<String, String> data) throws IOException{
+        URL url;
+        URLConnection urlConn;
+        DataOutputStream cgiInput;
+
+        // URL of target page script.
+        url = new URL("http://localhost:9000/StackxWS/ws");
+        urlConn = url.openConnection();
+
+        urlConn.setDoInput(true);
+        urlConn.setDoOutput(true);
+        urlConn.setUseCaches(false);
+        urlConn.setRequestProperty("Content-Type", "text/xml");
+
+        // Send POST output.
+        cgiInput = new DataOutputStream(urlConn.getOutputStream());
+
+        Iterator it = data.entrySet().iterator();
+        String params = "";
+
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry)it.next();
+
+            params += "<" + pair.getKey() + ">" + pair.getValue() + "</" + pair.getKey() + ">\n";
+        }
+
+        String content = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><S:Envelope xmlns:S=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:SOAP-ENV=\"http://schemas.xmlsoap.org/soap/envelope/\">\n" +
+                "    <SOAP-ENV:Header/>\n" +
+                "    <S:Body>\n" +
+                "        <ns2:" + method + " xmlns:ns2=\"http://stackx.me.org/\">\n" +
+                            params +
+                "        </ns2:" + method + ">\n" +
+                "    </S:Body>\n" +
+                "</S:Envelope>";
+
+//        return content;
+
+//        String content = "email=" + URLEncoder.encode(param1) + "&param2=" + URLEncoder.encode(param2);
+
+        cgiInput.writeBytes(content);
+        cgiInput.flush();
+        cgiInput.close();
+
+        // reads the CGI response and print it inside the servlet content
+        BufferedReader cgiOutput =new BufferedReader(new InputStreamReader(urlConn.getInputStream()));
+
+        String line = null;
+        String res = "";
+        while (null != (line = cgiOutput.readLine())){
+            res += line;
+        }
+
+        return res;
+    }
+}
