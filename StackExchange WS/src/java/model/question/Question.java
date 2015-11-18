96fa6cf62e2bd20c3f7a57a096f45f8cc9674ef2
Asanilta Fahda <asanilta@gmail.com>
2015-11-18 10:38:31 +0700

    Add QuestionWS & edit DB.java

---
+++ StackExchange WS/src/java/model/question/Question.java
@@ -0,0 +1,40 @@
+package model.question;
+
+import javax.xml.bind.*;
+import javax.xml.bind.annotation.*;
+
+/**
+ *
+ * @author Asanilta
+ */
+
+@XmlRootElement(name = "Question")
+public class Question {
+    
+    @XmlElement(name="question_id", required=true)
+    private int question_id;
+    @XmlElement(name="topic", required=true)
+    private String topic;
+    @XmlElement(name="content", required=true)
+    private String content;
+    @XmlElement(name="user_id", required=true)
+    private int user_id;
+    @XmlElement(name="create_time", required=true)
+    private String create_time;
+    @XmlElement(name="vote", required=true)
+    private int vote;
+    
+    public Question() {
+        question_id = 0;
+        user_id = 0;
+        vote = 0;
+    }
+    public Question(int question_id, String topic, String content, int user_id, String create_time, int vote) {
+        this.question_id = question_id;
+        this.topic = topic;
+        this.content = content;
+        this.user_id = user_id;
+        this.create_time = create_time;
+        this.vote = vote;
+    }
+}
---
+++ StackExchange WS/src/java/model/question/QuestionWS.java
@@ -0,0 +1,74 @@
+package model.question;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebResult;
+import connection.DB;
+import java.sql.Connection;
+import java.sql.Statement;
+import javax.jws.WebParam;
+
+/**
+ *
+ * @author Asanilta
+ */
+@WebService(serviceName = "QuestionWS")
+public class QuestionWS {
+    Connection conn = DB.connect();
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "getAllQuestions")
+    @WebResult(name="Question")
+    public ArrayList<Question> getAllQuestions() {
+        ArrayList<Question> questions = new ArrayList<>();
+        
+        try {
+                
+            Statement stmt = conn.createStatement();
+            String sql = "SELECT * FROM question";
+            
+            ResultSet rs = stmt.executeQuery(sql);
+            while (rs.next()) {
+                questions.add(new Question(rs.getInt("question_id"),rs.getString("topic"),rs.getString("content"),rs.getInt("user_id"),rs.getString("create_time"),rs.getInt("vote")));
+            }
+            
+            rs.close();
+            stmt.close();
+        } catch (SQLException ex) {
+            //Logger.getLogger(QuestionWS.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return questions;
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "getQuestion")
+    @WebResult(name="Question")
+    public Question getQuestion(@WebParam(name = "question_id") int question_id) {
+        Question question = null;
+        try {
+                
+            Statement stmt = conn.createStatement();
+            String sql = "SELECT * FROM question WHERE question_id="+question_id;
+            ResultSet rs = stmt.executeQuery(sql);
+            while (rs.next()) {
+                question = new Question(rs.getInt("question_id"),rs.getString("topic"),rs.getString("content"),rs.getInt("user_id"),rs.getString("create_time"),rs.getInt("vote"));
+            }
+            
+            rs.close();
+            stmt.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(QuestionWS.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return question;
+    }
+}
