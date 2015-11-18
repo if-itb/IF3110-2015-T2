68fa1d73aea9f8964b535974b87299bb2abb9cfe
Muhamad Visat Sutarno <mvisat@gmail.com>
2015-11-18 14:03:14 +0700

    Add login servlet and page

---
+++ StackExchange Client/src/java/controller/LoginServlet.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+/**
+ *
+ * @author visat
+ */
+public class LoginServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher("WEB-INF/view/login.jsp").forward(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        if (email == null || password == null) {
+            response.sendRedirect(request.getContextPath());
+            return;
+        }
+        URLConnection connection = new URL("http://localhost:8080/IdentityService/login").openConnection();        
+        connection.setDoOutput(true);
+        connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+        try (OutputStream output = connection.getOutputStream()) {
+            String charset = java.nio.charset.StandardCharsets.UTF_8.name();
+            String query = String.format(
+                    "email=%s&password=%s",
+                    URLEncoder.encode(email, charset),
+                    URLEncoder.encode(password, charset));
+            output.write(query.getBytes(charset));
+        }        
+        try {
+            JSONObject jsonResponse = (JSONObject)new JSONParser().parse(new InputStreamReader(connection.getInputStream()));            
+            for (Iterator iterator = jsonResponse.keySet().iterator(); iterator.hasNext();) {
+                String key = (String)iterator.next();
+                Cookie cookie = new Cookie(key, (String)jsonResponse.get(key));
+                response.addCookie(cookie);
+            }            
+        } catch (ParseException ex) {
+            Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        response.sendRedirect(request.getContextPath());
+}
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ StackExchange Client/web/WEB-INF/view/login.jsp
@@ -0,0 +1,18 @@
+<%-- 
+    Document   : login
+    Created on : Nov 18, 2015, 12:04:59 PM
+    Author     : visat
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<jsp:include page="header.jsp" flush="true"/>
+    <div class="container">
+        <h2>Login</h2>
+        <hr class="heading">
+        <form action="" method="post">            
+            <input type="text" name="email" placeholder="Email" required autofocus"></input>
+            <input type="text" name="password" placeholder="Password" required"></input>            
+            <input type="submit" class="btn-default btn-right" value="Login"></input>            
+        </form>
+    </div>
+<jsp:include page="footer.jsp" flush="true"/>
