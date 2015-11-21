edde04ff60fbefa0ebc4e60650958e210937beb2
Octavianus Marcel Harjono <octavianusmarcel12@gmail.com>
2015-11-21 18:09:34 +0700

    add json parser
    implement json praser with dummy json data consisting of username and
    password

--- Identity Service/src/java/Token/GenerateToken.java
+++ Identity Service/src/java/Token/GenerateToken.java
@@ -8,14 +8,24 @@ package Token;
 import database.Database;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 import java.util.Base64;
+import java.util.Calendar;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 /**
  *
@@ -37,6 +47,33 @@ public class GenerateToken extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
+            //Variable
+            //path and port for the database
+            String path = "jdbc:mysql://localhost:3306/stack_exchange";
+            
+            //soon implemented with HTTP Headers or something else
+            String username = "";
+            String password = "";
+            
+            String jsonString = "{\"username\":\"william\", \"password\":\"william\"}";
+            JSONParser parser = new JSONParser();
+            try {
+                Object obj = parser.parse(jsonString);
+                JSONObject obj2=(JSONObject) obj;
+                username = obj2.get("username").toString();
+                password = obj2.get("password").toString();
+                out.println(username + " " + password);
+            } catch (ParseException ex) {
+                Logger.getLogger(GenerateToken.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+            //query for database
+            String query = "SELECT COUNT(*) FROM user WHERE nama = '" + username + "' AND password = '" + password + "'";
+            
+            
+            //Lifetime
+            int lifetime = 30; //minutes
+            
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
@@ -48,14 +85,7 @@ public class GenerateToken extends HttpServlet {
             out.println("</body>");
             out.println("</html>");
             
-            //path and port for the database
-            String path = "jdbc:mysql://localhost:3306/stack_exchange";
-            
-            //soon implemented with HTTP Headers or something else
-            String usernameDummy = "william";
-            String passwordDummy = "william";
-            String query = "SELECT COUNT(*) FROM user WHERE nama = '" + usernameDummy + "' AND password = '" + passwordDummy + "'";
-            
+            //Check username and password in database
             Database database = new Database();
             database.connect(path);
             
@@ -67,18 +97,30 @@ public class GenerateToken extends HttpServlet {
                 rs.close();
             } catch (SQLException ex) {
             }
-            
             database.closeDatabase();
             
+            //Output token and lifetime
             if(result == 1){
                 //generating token
-                String base64encodedString = Base64.getEncoder().encodeToString(usernameDummy.getBytes("utf-8"));
-                out.println("Token :" + base64encodedString);
+                
+                String encoded = Base64.getEncoder().encodeToString(username.getBytes("utf-8"));
+                out.println("Token :" + encoded + "\n");
+                byte[] byteDecoded = Base64.getDecoder().decode(encoded);
+                String decoded = new String(byteDecoded, StandardCharsets.UTF_8);
+                out.println("Decoded : " + decoded);
+                out.println(generateLifetime(lifetime));
             } else {
                 //not generating token
-                out.println("Username does not exist or password does not match!");
+                out.println(result + " Username does not exist or password does not match!");
             }
         }
+    }
+    
+    public String generateLifetime(int lifetime){
+        Calendar now = Calendar.getInstance();
+        now.add(Calendar.MINUTE, lifetime);
+        SimpleDateFormat df = new SimpleDateFormat("HH:mm");
+        return (df.format(now.getTime()));
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
