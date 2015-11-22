8f22ce762e3c6dde504ca6d45724c943b25de417
Rahman Adianto <rahman.adianto@gmail.com>
2015-11-22 10:21:16 +0700

    Add Token Generator and Validator

---
+++ StackExchangeAuth/src/java/Call.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Form;
+import javax.ws.rs.core.MediaType;
+
+/**
+ *
+ * @author User
+ */
+@WebServlet(urlPatterns = {"/Call"})
+public class Call extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+        String uname = request.getParameter("uname");
+        String pass = request.getParameter("pass");
+        
+        Form form = new Form();
+        form.param("uname", uname);
+        form.param("pass", pass);
+       
+        Client client = ClientBuilder.newClient();
+        String url = "http://localhost:25973/StackExchangeAuth/Test";
+        
+        try {
+            String result = client.target(url).request(MediaType.TEXT_PLAIN).post(Entity.entity(form,
+                    MediaType.APPLICATION_FORM_URLENCODED), String.class);
+            
+            PrintWriter tw = response.getWriter();
+        
+            tw.println(result);
+        }
+        catch (Exception e) {
+            
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ StackExchangeAuth/src/java/Token/TokenGenerator.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Token;
+
+import Config.DB;
+import java.util.UUID;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author User
+ */
+public class TokenGenerator {
+    private String token;
+    
+    private static String getCurrentTimeStamp() {
+        Calendar cal = Calendar.getInstance();  
+	Timestamp now = new Timestamp(cal.getTimeInMillis());
+        return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(now); 
+    }
+    
+    public TokenGenerator() {
+        this.token = UUID.randomUUID().toString();
+        
+        DB db = new DB();
+        Connection conn = db.connect();
+        
+        try {
+            Statement stmt;
+            stmt = conn.createStatement();
+            String sql;
+            sql = "INSERT INTO token (token, date_create)VALUES (?, ?)";
+
+            PreparedStatement dbStatement = conn.prepareStatement(sql);
+            dbStatement.setString(1, token);
+            dbStatement.setString(4, getCurrentTimeStamp());
+            
+            dbStatement.execute();
+            
+            stmt.close();
+            conn.close();
+        }
+        catch(SQLException ex) {
+          
+        }
+    }
+    
+    public String getToken() {
+        return token;
+    }
+}
+
+
