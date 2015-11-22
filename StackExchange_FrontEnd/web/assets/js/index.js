288cd98d4c55a73e6e4460e25594b55e06f19370
elvanowen <vanzz_95@hotmail.com>
2015-11-22 12:25:57 +0700

    Added vote up and down functionality

--- StackExchange_FrontEnd/web/assets/js/index.js
+++ StackExchange_FrontEnd/web/assets/js/index.js
@@ -15,11 +15,43 @@
             $(this).find('.fixed-action-btn > .btn-floating').click();
         })
 
+        $('.modal-trigger').leanModal();
+
+        function getParameterByName(name) {
+            name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+            var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+                results = regex.exec(location.search);
+            return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+        }
+
         $('#recent-post .card').click(function(){
-            window.location.href="/answer?id=" + $(this).attr('data-id')
+            if (getParameterByName('token')){
+                window.location.href="/answer?token=" + getParameterByName('token') + "&id=" + $(this).attr('data-id')
+            } else{
+                window.location.href="/answer?id=" + $(this).attr('data-id')
+            }
+
         })
 
-        $('.modal-trigger').leanModal();
+        if (getParameterByName('token')){
+            $('#add-question-form').attr('action', '/ask?token=' + getParameterByName('token'));
+        } else {
+            $('#add-question-form').attr('action', '/ask');
+        }
+
+        $('.question-upvote-btn').click(function(e){
+            e.stopPropagation()
+            var id = $(this).closest('.card').attr('data-id')
+            $('<form action="/question/upvote?token=' + getParameterByName('token') + '" method="POST">' +
+            '<input type="hidden" name="id" value="' + id + '"/></form>').appendTo('body').submit();
+        })
+
+        $('.question-downvote-btn').click(function(e){
+            e.stopPropagation()
+            var id = $(this).closest('.card').attr('data-id')
+            $('<form action="/question/downvote?token=' + getParameterByName('token') + '" method="POST">' +
+            '<input type="hidden" name="id" value="' + id + '"/></form>').appendTo('body').submit();
+        })
 
 
     }); // end of document ready
