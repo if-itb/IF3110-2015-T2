ef2064a91c554cbbddc7d39d2260a0e2b360fbf5
MarcoOrlando <13513038@std.stei.itb.ac.id>
2015-11-23 11:48:25 +0700

    Answer SOAP  merged

--- StackExchange_WS/src/java/WSModel/Answer.java
+++ StackExchange_WS/src/java/WSModel/Answer.java
@@ -13,9 +13,10 @@ import java.sql.Timestamp;
 import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;  
-import WSModule.AnswerClass;
+import java.sql.Statement;
 
+import WSModule.AnswerClass;
+import java.util.ArrayList;
 /**
  *
  * @author Jessica
@@ -26,7 +27,7 @@ public class Answer {
     final static String USER = "root";
     final static String PASS = "";
     
-     public static Boolean addAnswer(int question_id, String answerContent, int answerVote, int userID ) {
+     public static Boolean addAnswer(int question_id, String answerContent, int userID ) {
         boolean success;
         Connection conn = null;
         Statement statement = null;
@@ -35,7 +36,7 @@ public class Answer {
             Class.forName(JDBC_DRIVER);
             conn = DriverManager.getConnection(localhost, USER, PASS);
             statement = conn.createStatement();
-            String query = "INSERT INTO answers(`question_id`, `content`, `vote`, `userID`, `date`) VALUES (" + question_id + ",\"" + answerContent + "\"," + answerVote + "," + userID + " ,now())";
+            String query = "INSERT INTO answers(`question_id`, `content`, `userID`, `date`) VALUES (" + question_id + ",'" + answerContent + "'," + userID + " ,now())";
             statement.executeUpdate(query);
             //System.out.println(query);
             success = true;
@@ -59,20 +60,30 @@ public class Answer {
         }
         return success;
     }
-    
-     /*
-    public static AnswerClass getAnswerByID(int answer_id) {
+     
+    public static ArrayList<AnswerClass> getAnswerByID(int answer_id) {
         Connection conn = null;
         Statement statement = null;
-        AnswerClass answer = null;
+        ArrayList<AnswerClass> answerList = new ArrayList<AnswerClass>();
+        AnswerClass[] answerArray = null;
         try {
             Class.forName(JDBC_DRIVER);
             conn = DriverManager.getConnection(localhost, USER, PASS);
-            statement = conn.createStatement();
             String query = "SELECT * FROM answers WHERE answer_id=" + answer_id;
+            statement = conn.prepareStatement(query, statement.RETURN_GENERATED_KEYS);
             ResultSet rs = statement.executeQuery(query);
-            if (rs.next()) {
-                answer = new AnswerClass(rs.getInt("answer_id"), rs.getInt("question_id"), rs.getString("content"), rs.getInt("vote"), rs.getString("date"), rs.getInt("userID"));
+        
+            while (rs.next()) {
+                //Retrieve by column name
+                int answerId  = rs.getInt("answer_id");
+                int question_id  = rs.getInt("question_Id");
+                String content = rs.getString("content");
+                int vote  = rs.getInt("vote");
+                String date = rs.getDate("date").toString();
+                int userID  = rs.getInt("userID");
+
+                AnswerClass answerRecord = new AnswerClass(answerId, question_id, content, vote, date, userID);
+                answerList.add(answerRecord);
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -91,12 +102,54 @@ public class Answer {
                 e.printStackTrace();
             }
         }
-        return answer;
+        return answerList;
     }
     
-*/
+    public static ArrayList<AnswerClass> getAnswerByQID(int question_id) {
+        Connection conn = null;
+        Statement statement = null;
+        ArrayList<AnswerClass> answerList = new ArrayList<AnswerClass>();
+        AnswerClass[] answerArray = null;
+        try {
+            Class.forName(JDBC_DRIVER);
+            conn = DriverManager.getConnection(localhost, USER, PASS);
+            String query = "SELECT * FROM answers WHERE question_id=" + question_id;
+            statement = conn.prepareStatement(query, statement.RETURN_GENERATED_KEYS);
+            ResultSet rs = statement.executeQuery(query);
+        
+            while (rs.next()) {
+                //Retrieve by column name
+                int answer_id  = rs.getInt("answer_Id");
+                int questionId  = rs.getInt("question_id");
+                String content = rs.getString("content");
+                int vote  = rs.getInt("vote");
+                String date = rs.getDate("date").toString();
+                int userID  = rs.getInt("userID");
 
-    public static Boolean voteUpAnswer(int question_id) {
+                AnswerClass answerRecord = new AnswerClass(answer_id, questionId, content, vote, date, userID);
+                answerList.add(answerRecord);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }               
+        if (statement != null) {
+            try {
+                statement.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        if (conn != null) {
+            try {
+                conn.close();    
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return answerList;
+    }
+    
+    public static Boolean voteUpAnswer(int answer_id) {
         boolean success;
         Connection conn = null;
         Statement statement = null;
@@ -105,7 +158,7 @@ public class Answer {
             Class.forName(JDBC_DRIVER);
             conn = DriverManager.getConnection(localhost, USER, PASS);
             statement = conn.createStatement();
-            String query = "UPDATE answers SET vote = vote+1 WHERE question_id = " + question_id;
+            String query = "UPDATE answers SET vote = vote+1 WHERE answer_id = " + answer_id;
             statement.executeUpdate(query);
             success = true;
         } catch (Exception e) {
@@ -129,7 +182,7 @@ public class Answer {
         return success;
     }
     
-    public static Boolean voteDownAnswer(int question_id) {
+    public static Boolean voteDownAnswer(int answer_id) {
         boolean success;
         Connection conn = null;
         Statement statement = null;
@@ -138,7 +191,7 @@ public class Answer {
             Class.forName(JDBC_DRIVER);
             conn = DriverManager.getConnection(localhost, USER, PASS);
             statement = conn.createStatement();
-            String query = "UPDATE answers SET vote = vote-1 WHERE question_id = " + question_id;
+            String query = "UPDATE answers SET vote = vote-1 WHERE answer_id = " + answer_id;
             statement.executeUpdate(query);
             success = true;
         } catch (Exception e) {
