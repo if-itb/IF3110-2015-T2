4478d866eebd44258b9094244946349440698956
David <davidkwan95@gmail.com>
2015-11-26 12:25:21 +0700

    Register WebService created

---
+++ StackExchangeWS/src/java/stackExchangeWS/services/UserHandler.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stackExchangeWS.services;
+
+import java.sql.SQLException;
+import javax.jws.Oneway;
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import stackExchangeWS.database.DbManager;
+import stackExchangeWS.database.User;
+
+/**
+ *
+ * @author davidkwan
+ */
+@WebService(serviceName = "UserHandler")
+public class UserHandler {
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "registerUser")
+    public int registerUser(@WebParam(name = "email") String email, @WebParam(name = "password") String password) throws SQLException {
+        User user = new User();
+        user.setEmail(email);
+        user.setPassword(password);
+        if(DbManager.isUserExist(user)){
+            // User already exist, return -1
+            return -1;
+        }
+        else{
+            DbManager.registerUser(user);
+            // Registration successful, return 1
+            return 1;
+        }
+    }
+    
+}
