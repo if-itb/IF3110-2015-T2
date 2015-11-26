2a921542078e58c2c56945e5f9f108177991e50d
Juan Anton <juan.anton.che@gmail.com>
2015-11-26 16:03:08 +0700

    expiration date and validation service

--- IdentityService/src/java/LoginServlet.java
+++ IdentityService/src/java/LoginServlet.java
@@ -6,6 +6,9 @@
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.UUID;
 import java.util.logging.*;
 import javax.servlet.ServletException;
@@ -30,22 +33,6 @@ public class LoginServlet extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet LoginServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet LoginServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
@@ -61,10 +48,8 @@ public class LoginServlet extends HttpServlet {
             throws ServletException, IOException {
         PrintWriter out = response.getWriter();
         
-        out.println("Servlet");
-        response.sendRedirect("http://localhost:8081/Front-End/login.jsp");
     }
-
+    
     /**
      * Handles the HTTP <code>POST</code> method.
      *
@@ -80,61 +65,57 @@ public class LoginServlet extends HttpServlet {
         
         DBConnection connection = new DBConnection();
         Connection conn = connection.getConn();
-        if (request.getParameter("token") != null) {
-            String checkToken = "SELECT * from token WHERE value = '" + request.getParameter("token") + "'";
-            
+        if (request.getParameter("email") != null && request.getParameter("password") != null) {
+                
+            String query1 = "SELECT * from user WHERE email = '" + request.getParameter("email") + "' AND password = '" + request.getParameter("password") + "'";
+            String query2 = "SELECT * from user WHERE email = '" + request.getParameter("email") + "'";
+            String query3 = "SELECT * from token WHERE user_Email = '" + request.getParameter("email") + "'";
+
             try {
                 statement = conn.createStatement();
-                ResultSet rs = statement.executeQuery(checkToken);
+                ResultSet rs = statement.executeQuery(query3);
+
                 if (rs.next()) {
-                    request.setAttribute("tokenStatus", 1);
+                    String DeleteQuery = "DELETE FROM token WHERE user_Email = '" + request.getParameter("email") + "'";
+                    statement.execute(DeleteQuery);
                 }
-                
+
+                rs = statement.executeQuery(query1);
+
+                if (rs.next()) {
+                    UUID tokenGenerator = UUID.randomUUID();
+                    String token = tokenGenerator.toString();
+
+                    Calendar date = Calendar.getInstance();
+                    long t = date.getTimeInMillis();
+                    Date expirationDate = new Date(t + (5 * 60000));
+                    DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+
+                    String updateQuery = "INSERT INTO token(value, user_Email, expiration_date) values('" + token + "', '" + request.getParameter("email") + "', '" + df.format(expirationDate) + "')";
+                    statement.execute(updateQuery);
+                    response.sendRedirect("http://localhost:8082/Front-End/index.jsp?token=" + token);
+                }
+                else {
+                    rs = statement.executeQuery(query2);
+                    if (rs.next()) {
+                        response.sendRedirect("http://localhost:8082/Front-End/login.jsp?fail");
+                    }
+                    else {
+                        response.sendRedirect("http://localhost:8082/Front-End/login.jsp?fail");
+                    }
+                }
+
                 rs.close();
                 statement.close();
-                
+
             } catch (SQLException ex) {
                 Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
             }
-            
         }
-        else {
-            if (request.getParameter("email") != null && request.getParameter("password") != null) {
-                
-                String query1 = "SELECT * from user WHERE email = '" + request.getParameter("email") + "' AND password = '" + request.getParameter("password") + "'";
-                String query2 = "SELECT * from user WHERE email = '" + request.getParameter("email") + "'";
-                
-                try {
-                    statement = conn.createStatement();
-                    ResultSet rs = statement.executeQuery(query1);
-                    if (rs.next()) {
-                        UUID tokenGenerator = UUID.randomUUID();
-                        String token = tokenGenerator.toString();
-                        String updateQuery = "INSERT INTO token(value, user_Email) values('" + token + "', '" + request.getParameter("email") + "')";
-                        statement.execute(updateQuery);
-                        response.sendRedirect("http://localhost:8081/Front-End/index.jsp?token=" + token);
-                    }
-                    else {
-                        rs = statement.executeQuery(query2);
-                        if (rs.next()) {
-                            response.sendRedirect("http://localhost:8081/Front-End/login.jsp?fail1");
-                        }
-                        else {
-                            response.sendRedirect("http://localhost:8081/Front-End/login.jsp?fail");
-                        }
-                    }
-                    
-                    rs.close();
-                    statement.close();
-
-                } catch (SQLException ex) {
-                    Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
-                }
-            }
             
-            else {
-                response.sendRedirect("http://localhost:8081/Front-End/login.jsp");
-            }
+        else {
+            response.sendRedirect("http://localhost:8082/Front-End/login.jsp");
         }
     }
 
