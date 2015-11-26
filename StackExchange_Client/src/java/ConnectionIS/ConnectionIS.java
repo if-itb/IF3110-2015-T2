eb5be0814b586a10bb9215180d68637892723851
fitrakun <fitrakun@gmail.com>
2015-11-26 17:30:48 +0700

    update connectionIS, add JSON simple library

--- StackExchange_Client/src/java/ConnectionIS/ConnectionIS.java
+++ StackExchange_Client/src/java/ConnectionIS/ConnectionIS.java
@@ -5,82 +5,80 @@
  */
 package ConnectionIS;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.PrintWriter;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 /**
  *
- * @author HP
+ * @author visat
  */
-public class ConnectionIS extends HttpServlet {
+public class ConnectionIS {
+    private static final String CONTEXT_PATH = "http://localhost:8082/Identity_Service";
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ConnectionIS</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ConnectionIS at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+    private static JSONObject request(String servletPath, byte[] query) {
+        JSONObject jo = null;
+        try {
+            if (servletPath != null && !servletPath.startsWith("/"))
+                servletPath = "/" + servletPath;
+            URLConnection connection = new URL(CONTEXT_PATH + servletPath).openConnection();
+            connection.setDoOutput(true);
+            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            try (OutputStream output = connection.getOutputStream()) {
+                output.write(query);
+            }
+            StringBuilder builder = new StringBuilder();
+            BufferedReader buf = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String read;
+            while ((read = buf.readLine()) != null)
+                builder.append(read);            
+            jo = (JSONObject)new JSONParser().parse(builder.toString());            
         }
+        catch (IOException ex) {
+            System.err.println(ex.getMessage());
+        } catch (ParseException ex) {
+            Logger.getLogger(ConnectionIS.class.getName()).log(Level.SEVERE, null, ex);
+        }        
+        return jo;
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
+    public static JSONObject requestLogin(String email, String password) {
+        JSONObject jo = null;
+        try {
+            String charset = java.nio.charset.StandardCharsets.UTF_8.name();
+            String query = String.format(
+                    "email=%s&password=%s",
+                    URLEncoder.encode(email, charset),
+                    URLEncoder.encode(password, charset));
+            jo = request("/login", query.getBytes());
+        } catch (Exception ex) {
+            System.err.println(ex.getMessage());
+        }
+        return jo;
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
+    public static JSONObject requestAuth(String token_id) {
+        JSONObject jo = null;
+        try {
+            String charset = java.nio.charset.StandardCharsets.UTF_8.name();
+            String query = String.format(
+                    "auth=%s",
+                    URLEncoder.encode(token_id, charset));
+            jo = request("/auth", query.getBytes());
+        } catch (Exception ex) {
+            System.err.println(ex.getMessage());
+        }
+        return jo;
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
 }
