b4642d28fb07a2a7d8f6a83d81c447708c6e5211
Juan Anton <juan.anton.che@gmail.com>
2015-11-26 16:04:33 +0700

    updated database

--- restingsoapdb.sql
+++ restingsoapdb.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.12
+-- version 4.4.14
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 26, 2015 at 08:36 AM
--- Server version: 5.6.16
--- PHP Version: 5.5.11
+-- Generation Time: Nov 26, 2015 at 09:56 AM
+-- Server version: 5.6.26-log
+-- PHP Version: 5.6.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `restingsoapdb`
@@ -27,17 +27,13 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `answer` (
-  `A_ID` int(3) NOT NULL AUTO_INCREMENT,
+  `A_ID` int(3) NOT NULL,
   `Q_ID` int(3) NOT NULL,
   `Content` varchar(12000) NOT NULL,
   `Vote` int(3) NOT NULL,
   `Time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `User_ID` int(20) NOT NULL,
-  PRIMARY KEY (`A_ID`),
-  KEY `Q_ID` (`Q_ID`),
-  KEY `User_ID` (`User_ID`),
-  KEY `User_ID_2` (`User_ID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+  `User_ID` int(20) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -46,17 +42,14 @@ CREATE TABLE IF NOT EXISTS `answer` (
 --
 
 CREATE TABLE IF NOT EXISTS `question` (
-  `Q_ID` int(3) NOT NULL AUTO_INCREMENT,
+  `Q_ID` int(3) NOT NULL,
   `Vote` int(3) NOT NULL,
   `Answer_Count` int(11) NOT NULL DEFAULT '0',
   `Time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `Title` varchar(30) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `Content` varchar(12000) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `User_ID` int(20) NOT NULL,
-  PRIMARY KEY (`Q_ID`),
-  UNIQUE KEY `Q_ID` (`Q_ID`),
-  KEY `User_ID` (`User_ID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+  `User_ID` int(20) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `question`
@@ -73,18 +66,18 @@ INSERT INTO `question` (`Q_ID`, `Vote`, `Answer_Count`, `Time`, `Title`, `Conten
 --
 
 CREATE TABLE IF NOT EXISTS `token` (
-  `Token_ID` int(100) NOT NULL AUTO_INCREMENT,
+  `Token_ID` int(100) NOT NULL,
   `value` varchar(2000) NOT NULL,
   `user_Email` varchar(3000) NOT NULL,
-  PRIMARY KEY (`Token_ID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+  `expiration_date` varchar(3000) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `token`
 --
 
-INSERT INTO `token` (`Token_ID`, `value`, `user_Email`) VALUES
-(1, '5d100823-9364-4018-95a4-6495538e4e9d', 'benbong@gmail.com');
+INSERT INTO `token` (`Token_ID`, `value`, `user_Email`, `expiration_date`) VALUES
+(2, '66931c37-c8dd-4442-8364-60be65f4b5bb', 'benbong@gmail.com', '2015-11-26 15:40:27');
 
 -- --------------------------------------------------------
 
@@ -93,12 +86,11 @@ INSERT INTO `token` (`Token_ID`, `value`, `user_Email`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `user` (
-  `User_ID` int(20) NOT NULL AUTO_INCREMENT,
+  `User_ID` int(20) NOT NULL,
   `Name` varchar(15000) NOT NULL,
   `Email` varchar(2000) NOT NULL,
-  `Password` varchar(3000) NOT NULL,
-  PRIMARY KEY (`User_ID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+  `Password` varchar(3000) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
@@ -110,6 +102,63 @@ INSERT INTO `user` (`User_ID`, `Name`, `Email`, `Password`) VALUES
 (3, 'Juan Anton', 'juan_anton@gmail.com', 'juananton');
 
 --
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `answer`
+--
+ALTER TABLE `answer`
+  ADD PRIMARY KEY (`A_ID`),
+  ADD KEY `Q_ID` (`Q_ID`),
+  ADD KEY `User_ID` (`User_ID`),
+  ADD KEY `User_ID_2` (`User_ID`);
+
+--
+-- Indexes for table `question`
+--
+ALTER TABLE `question`
+  ADD PRIMARY KEY (`Q_ID`),
+  ADD UNIQUE KEY `Q_ID` (`Q_ID`),
+  ADD KEY `User_ID` (`User_ID`);
+
+--
+-- Indexes for table `token`
+--
+ALTER TABLE `token`
+  ADD PRIMARY KEY (`Token_ID`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`User_ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `answer`
+--
+ALTER TABLE `answer`
+  MODIFY `A_ID` int(3) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `question`
+--
+ALTER TABLE `question`
+  MODIFY `Q_ID` int(3) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `token`
+--
+ALTER TABLE `token`
+  MODIFY `Token_ID` int(100) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `User_ID` int(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
 -- Constraints for dumped tables
 --
 
@@ -117,8 +166,8 @@ INSERT INTO `user` (`User_ID`, `Name`, `Email`, `Password`) VALUES
 -- Constraints for table `answer`
 --
 ALTER TABLE `answer`
-  ADD CONSTRAINT `answer_ibfk_2` FOREIGN KEY (`User_ID`) REFERENCES `user` (`User_ID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `answer_ibfk_1` FOREIGN KEY (`Q_ID`) REFERENCES `question` (`Q_ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `answer_ibfk_1` FOREIGN KEY (`Q_ID`) REFERENCES `question` (`Q_ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `answer_ibfk_2` FOREIGN KEY (`User_ID`) REFERENCES `user` (`User_ID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `question`
