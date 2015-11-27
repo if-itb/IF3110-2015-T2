8bbe3b6a1b87ffd63f8720250e96fbe3c4faff32
Muhamad Fikri Alhawarizmi <mfikria@gmail.com>
2015-11-27 21:52:39 +0700

    fix Identity service

--- SimpleStackExchange_IdentityService/build/generated-sources/ap-source-output/entity/Activeuser_.java
+++ SimpleStackExchange_IdentityService/build/generated-sources/ap-source-output/entity/Activeuser_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-11-26T08:14:11")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-11-27T14:21:10")
 @StaticMetamodel(Activeuser.class)
 public class Activeuser_ { 
 
